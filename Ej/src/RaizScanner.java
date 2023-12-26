418dc6d06a90e3a57f3edd5139b290ae9bbc6c4c
daniel soto <daniel.soto.ortiz@gmail.com>
2023-12-26 22:39:21 +0100

    first

---
+++ Ej/src/RaizScanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RaizScanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner tec = new Scanner(System.in);
+    }
+}
+
