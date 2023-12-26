bc0e88cc97918442fcbb8542133c30c760b98f1a
daniel soto <daniel.soto.ortiz@gmail.com>
2023-12-26 22:46:20 +0100

    first

--- Ej/src/RaizScanner.java
+++ Ej/src/RaizScanner.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class RaizScanner {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         Scanner tec = new Scanner(System.in);
+        System.out.println("Introduce un número");
+        int a;
+        a=tec.nextInt();
+        double b;
+        b = Math.sqrt(a);
+        System.out.println("La raíz cuadrada de "+a+" es "+b);
+
+
     }
 }
 
